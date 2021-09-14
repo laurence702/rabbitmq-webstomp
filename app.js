3b3a27f1321a04b8375adc2f94399182ef39d3de
laurence702 <akaigbokwelaurence@gmail.com>
2021-09-14 10:02:21 +0100

    create exchange and queue in rabbitMQ admin..message sent successfully

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
-const express = require('express');
-const webstomp = require('webstomp-client');
-const websocket = require('ws');
+const express = require("express");
+const webstomp = require("webstomp-client");
+const websocket = require("ws");
 
-const client = webstomp.over(new  websocket('ws://127.0.0.1:15674/ws'));
 const app = express();
 port = 5000;
+const client = webstomp.over(new websocket("ws://127.0.0.1:15674/ws"));
 
-let message = 'I am connected to rabbitMQ using webstomp';
+let message = "I am connected to rabbitMQ using webstomp";
 
-function on_error(user, error) {
-    console.log('Disconnected',user.name, error);
-};
+function on_error(error) {
+  console.log("Disconnected", error);
+}
 
-function on_connect(user) {
-    console.log('connected', user.name);
-    client.send('/exchange/web-service-endpoint', message, {'content-type':'text/plain'} );
-};
+function on_connect(user) { 
+  console.log("connected", client);
+  client.send("/exchange/web-service-endpoint", message, {
+    "content-type": "text/plain",
+  });
+}
 
-client.connect('guest', 'guest', on_connect, on_error);
+client.connect("guest", "guest", on_connect, on_error);
 
 
-app.listen(port,()=>{
-    console.log(`App listening on port ${port}`);
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
 });
